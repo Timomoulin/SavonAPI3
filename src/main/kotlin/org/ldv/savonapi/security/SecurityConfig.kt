af2209151754d9628f79ffeef230aa5cf713fb89
T.Moulin <timomoulin@msn.com>
2026-03-17 11:17:26 +0100

    fix cors pour options

--- src/main/kotlin/org/ldv/savonapi/security/SecurityConfig.kt
+++ src/main/kotlin/org/ldv/savonapi/security/SecurityConfig.kt
@@ -3,6 +3,7 @@ package org.ldv.savonapi.security
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -35,6 +36,7 @@ class SecurityConfig(
 
         http
             .csrf { it.disable() }
+            .cors(Customizer.withDefaults<>())
             .sessionManagement {
                 it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             }
